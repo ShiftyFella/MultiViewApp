792f4d2c072478375ff2ba0aff77d2d49ef5f9e5
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-11-29 20:54:31 -0500

    segue to fontlist controller

--- MultiViewApp/RootTableViewController.swift
+++ MultiViewApp/RootTableViewController.swift
@@ -65,5 +65,20 @@ class RootTableViewController: UITableViewController {
         }
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indexPath = tableView.indexPath(for: sender as! UITableViewCell)!
+        let listVC = segue.destination as! FontListViewController
+        
+        if indexPath.section == 0 {
+            let familyName = familyNames[indexPath.row]
+            listVC.fontNames = (UIFont.fontNames(forFamilyName: familyName) as [String]).sorted()
+            listVC.navigationItem.title = familyName
+            listVC.showsFavorites = false
+        } else {
+            listVC.fontNames = favoritesList.favorites
+            listVC.navigationItem.title = "Favorites"
+            listVC.showsFavorites = true
+        }
+    }
     
 }
